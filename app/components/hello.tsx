217284937f1b17b9404504cd7fd5ebe405aa78d9
McQuinTrix <harshal.carpenter@gmail.com>
2025-09-29 23:36:50 -0700

    chore: nextjs init

---
+++ app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+function Hello() {
+    console.log("Hello I am a component");
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Hello;
